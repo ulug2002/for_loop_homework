e08771dd2b911997ee9110aab4e42c413e21f270
ulug2002 <ulugbek_it@mail.ru>
2023-05-03 15:01:26 +0300

    d1

--- For01.py
+++ For01.py
@@ -1,6 +1,3 @@
-import py_compile
-
-
 def main(n):
     """
     Return numbers from zero to n in a list view.
@@ -9,4 +6,9 @@ def main(n):
     Returns:
         list: return  answer
     """
-    return
+    l = []
+    for i in range(n):
+        l +=[i]
+        
+    return l
+print(main(5))
