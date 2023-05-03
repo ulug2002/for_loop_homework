9cdd108e5cb533ef6c02b0a351e24f388a8a5294
ulug2002 <ulugbek_it@mail.ru>
2023-05-03 15:03:49 +0300

    d7

--- For07.py
+++ For07.py
@@ -6,4 +6,10 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    a = 0
+    for i in range(N):
+        if i%2==1:
+            a+=i
+        
+    return a
+print(main(5))
